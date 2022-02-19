4d97ff7059d145c67ac4a04beecb0ee5e818254b
HelbertAguiar <helbert.aguiar@hotmail.com>
2022-02-19 19:36:48 -0300

    refator: update "cef_previdencia.py" por "main.py"

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from selenium.webdriver.firefox.options import Options
 from selenium import webdriver
+from bs4 import BeautifulSoup
 import logging
 import sys
+from time import sleep
+import os
 
 
 class ScraperPrev():
@@ -11,8 +13,8 @@ class ScraperPrev():
         self.lista_datas = self.busca_data()
         self.url = url
         self.sleep = sleep
-        self.browser = None
         self.inicia_navegador()
+        self.inicia_scraping()
         self.encerra_navegador()
 
     @staticmethod
@@ -20,7 +22,7 @@ class ScraperPrev():
         logging.basicConfig(
             filename='./log/log.txt',
             filemode='w',
-            level=logging.DEBUG,
+            level=logging.INFO,
             format=('%(asctime)s.%(msecs)03d'
                     '%(levelname)8s %(module)s: %(message)s'),
             datefmt='%Y-%m-%d %H:%M:%S'
@@ -28,44 +30,80 @@ class ScraperPrev():
 
     def busca_data(self):
         lista_datas = []
-        with open('./data/input', 'r') as reader:
+        with open('./data/input.txt', 'r') as reader:
             for line in reader.readlines():
                 lista_datas.append(line.split())
 
-        logging.info('Processo de buscar datas concluido')
+        logging.info('Concluido busca das datas para pesquisa')
         return lista_datas
+
+    def inicia_scraping(self):
+        driver = self.driver
+        driver.get(self.url)
+        sleep(self.sleep)
+        assert 'Caixa Vida e Previdência' in driver.title
+
+        with open('data/output.txt', 'a', encoding='utf-8') as file:
+            file.write('DATA;NOME FUNDO;TAXA ADM;COTA;CLASSE')
+            file.write("\n")
+
+        for data in self.lista_datas:
+            data_string = str(data).replace('[', '')\
+                                   .replace(']', '').replace('\'', '')
+            self.insere_data(driver, data_string)
+            sleep(self.sleep)
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
+            table = soup.find_all('table', {'class':
+                                            'tabela-fundo-investimento'})[0]
+            table = table.tbody
+            self.get_data(table, data_string)
+
+    def insere_data(self, driver, dataInserir):
+        sleep(self.sleep)
+        input = driver.find_element_by_id(
+            'formTabelaFundo.dataConsulta.input')
+        input.clear()
+        input.send_keys(dataInserir.replace('/', ''))
+
+    def get_data(self, table, data):
+
+        tags_tr = table.findChildren("tr", recursive=False)
+        classe = ''
+
+        for tr in tags_tr:
+            if tr.has_attr('class'):
+                classe = tr.text
+            else:
+                nome_fundo = tr.findAll('td')[0].text
+                taxa_adm = tr.findAll('td')[2].text
+                # data_cota = tr.findAll('td')[3].text
+                cota = tr.findAll('td')[4].text
+                self.save_to_file(data, nome_fundo,
+                                  taxa_adm, cota, classe)
+        logging.info(f'Consulta com sucesso referente a:{data}')
+
+    def save_to_file(self, data_cota, nome_fundo, taxa_adm, cota, classe):
+        with open('data/output.txt', 'a', encoding='utf-8') as file:
+            file.write(f'{data_cota};{nome_fundo};{taxa_adm};{cota};{classe}')
+            file.write("\n")
 
     def inicia_navegador(self):
         try:
-            options = Options()
-            options.binary_location = ('C:/Program Files/'
-                                       'Mozilla Firefox/firefox.exe')
-            self.browser = webdriver.Firefox(
-                options=options, executable_path='./driver/geckodriver.exe')
+            self.driver = webdriver.Firefox(
+                            executable_path='./driver/geckodriver.exe',
+                            service_log_path=os.devnull)
+            logging.info('Navegador aberto com sucesso')
         except:
             logging.critical('Falha na carga do driver do navegador')
             sys.exit('Falha no carregamento do driver ou localizacao '
                      'do path firefox')
 
-        logging.info('Driver do navegador iniciado')
-        self.browser.implicitly_wait(self.sleep)
-        self.browser.get(self.url)
-
-        if self.browser.title == '502 Bad Gateway':
-            self.browser.quit()
-            logging.critical('Falha no carga da pagina, error: 502 BADGATEWAY')
-            sys.exit('Falha no carregamento da pagina, error: 502 BAD GATEWAY')
-
-        logging.info('Pagina aberta com sucesso')
-        return
-
     def encerra_navegador(self):
-        self.browser.quit()
+        self.driver.close()
         logging.info('Navegador encerrado com sucesso')
 
 
-URL = 'https://www.caixavidaeprevidencia.com.br/ \
-        previdencia/rendimento-dos-fundos'
-SLEEP = 6
-
+URL = ('https://www.caixavidaeprevidencia.com.br/'
+       'previdencia/rendimento-dos-fundos')
+SLEEP = 12
 scraper = ScraperPrev(URL, SLEEP)
